69c7aaf14ab780a048944d6113af7b19ac0ecda6
ayeasin14 <ayeasin14@gmail.com>
2022-10-11 15:34:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BeakerIcon } from '@heroicons/react/24/solid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <BeakerIcon className="h-6 w-6 text-blue-500" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
