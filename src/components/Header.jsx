cec761f63b3f87ab5005d84d994fe2bab2cdd446
ayeasin14 <ayeasin14@gmail.com>
2022-10-12 15:36:06 +0600

    quiz cart added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 const Header = () => {
     const [open, setOpen] = useState(false)
     return (
         <div className='bg-orange-100 md:py-2 py-1 md:px-14 sm:px-10 px-3 flex justify-between items-center shadow-2xl'>
-            <div className='flex items-center my-3'>
+            <div className=''>
+                <Link to='/' className='flex items-center my-3'>
+                    <svg
+                        class="w-8 text-teal-accent-400 mx-3"
+                        viewBox="0 0 24 24"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        stroke="currentColor"
+                        fill="none"
+                    >
+                        <rect x="3" y="1" width="7" height="12" />
+                        <rect x="3" y="17" width="7" height="6" />
+                        <rect x="14" y="1" width="7" height="6" />
+                        <rect x="14" y="11" width="7" height="12" />
+                    </svg>
 
-                <svg
-                    class="w-8 text-teal-accent-400 mx-3"
-                    viewBox="0 0 24 24"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    stroke="currentColor"
-                    fill="none"
-                >
-                    <rect x="3" y="1" width="7" height="12" />
-                    <rect x="3" y="17" width="7" height="6" />
-                    <rect x="14" y="1" width="7" height="6" />
-                    <rect x="14" y="11" width="7" height="12" />
-                </svg>
-
-                <p className='md:text-3xl sm:text-2xl text-light font-extrabold	 text-green-500 font-sans'>Quiz Bucket</p>
+                    <p className='md:text-3xl sm:text-2xl text-light font-extrabold hover:text-lime-700 text-green-500 font-sans'>Quiz Bucket</p></Link>
             </div>
             <div className={`flex justify-end items-center md:gap-8 sm:gap-5 gap-2 absolute duration-500 md:static ${open ? 'sm:left-56 left-32' : 'left-[-300px]'}`}>
                 <NavLink className={({ isActive }) => isActive ? 'bg-green-200 duration-300 text-black md:px-3 sm:px-2 px-1 md:py-2 py-0 rounded text-sm md:font-semibold sm:font-normal font-light font-sans' : 'bg-green-500 duration-300 hover:bg-lime-700 md:px-3 sm:px-2 px-1 md:py-2 py-0 rounded text-white text-sm md:font-semibold sm:font-normal font-light font-sans'} to='/home'>Home</NavLink>
--- src/components/QuizCart.jsx
+++ src/components/QuizCart.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const QuizCart = () => {
+const QuizCart = ({ item }) => {
     return (
-        <div>
-
+        <div className='bg-slate-300 bg-opacity-80 relative rounded-md p-4 shadow-neutral-400 shadow-lg'>
+            <div>
+                <img src={item.logo} alt="" />
+            </div>
+            <div>
+                <p className='bg-slate-700 inline text-gray-300 text-xs py-1 px-2 rounded-lg my-1 absolute top-1 right-2'>Total quiz: {item.total}</p>
+            </div>
+            <div className='flex justify-between items-center'>
+                <p className='text-2xl bg-slate-200 rounded my-2 py-1 px-7 font-bold font-sans text-slate-700'>{item.name}</p>
+                <Link to={`/quiz/${item.id}`}><button className='bg-green-500 py-1 px-7 text-lg hover:bg-lime-700 duration-300 text-white rounded'>Start</button></Link>
+            </div>
         </div>
     );
 };
